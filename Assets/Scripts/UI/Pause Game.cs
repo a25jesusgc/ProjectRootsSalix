48300cc6e7156f25d7d50973c520bb4ecef2285c
Adrian <a25adrianrs@iessanclemente.net>
2026-04-08 18:25:54 +0200

    System Pause Fixed

--- Assets/Scripts/UI/Pause Game.cs
+++ Assets/Scripts/UI/Pause Game.cs
@@ -4,7 +4,8 @@ public class PauseGame : MonoBehaviour
 {
     public GameObject pauseMenu;
     public bool isPaused = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
     void Start()
     {
 
@@ -13,30 +14,44 @@ public class PauseGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // Detectar si se presiona la tecla de pausa (Escape)
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            // Si el juego ya está pausado, reanudarlo
             if (isPaused)
             {
+
                 ResumeGame();
             }
-            else
+            // Si el juego no está pausado, pausarlo
+            else if (!GlobalUtils.pause)
             {
                 Pause();
             }
         }
     }
 
+    // Método para reanudar el juego
     public void ResumeGame()
     {
+        // Desactivar el menú de pausa y reanudar el tiempo
+        GlobalUtils.pause = false;
+
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+
     }
 
+    // Método para pausar el juego
     public void Pause()
     {
+        // Activar el menú de pausa y detener el tiempo
+        GlobalUtils.pause = true;
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+
     }
 }
