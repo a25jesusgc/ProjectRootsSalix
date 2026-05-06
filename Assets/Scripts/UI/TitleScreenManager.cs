8eed70affc702ec3472f2e13c40769fabe3b1c85
a25jesusgc <a25jesusgc@iessanclemente.net>
2026-05-06 18:56:41 +0200

    New Game confirmation window

--- Assets/Scripts/UI/TitleScreenManager.cs
+++ Assets/Scripts/UI/TitleScreenManager.cs
@@ -1,15 +1,25 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreenManager : MonoBehaviour
 {
+    [SerializeField] private GameObject newGameWindow;
     [SerializeField] private GameObject settingsWindow;
+    [SerializeField] private Button continueButton;
     [SerializeField] private AudioLoop music;
+
+    private bool hasSaveFile;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         AudioManager.instance.PlayMusic(music);
+        
+        // Comprueba si existe partida guardada
+        hasSaveFile = File.Exists(GlobalUtils.SAVE_PATH);
+        continueButton.interactable = hasSaveFile;
     }
 
     // Update is called once per frame
@@ -18,12 +28,36 @@ public class TitleScreenManager : MonoBehaviour
         
     }
 
+    // Selección de botón de nueva partida
+    public void NewGameSelected()
+    {
+        
+        // Si ya existe guardado, pide confirmación
+        if (hasSaveFile)
+        {
+            ShowNewGameWarning(true);
+        }
+        else
+        {
+            // Si no hay partida guardada, inicia partida sin confirmación
+            NewGame();
+        }
+    }
+
+    // Muestra u oculta la ventana de confirmación de nueva partida
+    public void ShowNewGameWarning(bool show)
+    {
+        newGameWindow.SetActive(show);
+    }
+
+    // Inicia una nueva partida
     public void NewGame()
     {
         PlayerData.ResetInstance();
         SceneManager.LoadScene("WorldScene");
     }
 
+    // Continua la partida guardada
     public void Continue()
     {
         SceneManager.LoadScene("WorldScene");
