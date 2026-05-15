c6eb933e60633797416e73ad20a2ce4c35f96236
a25jesusgc <a25jesusgc@iessanclemente.net>
2026-05-15 18:34:24 +0200

    Currency fix

--- Assets/Scripts/Props/Currency.cs
+++ Assets/Scripts/Props/Currency.cs
@@ -10,7 +10,7 @@ public class Currency : MonoBehaviour
     // Valores para gestionar su desplazamiento al ser instanciado
     private Vector2 startSpeed;
     private float startRotation;
-    private float duration = 0.5f;
+    private float duration = 1f;
     private float timer;
 
 
